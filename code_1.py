3d6726b2d51824a3b66e22566c5fa8a5857ded2d
yanasekota <09041150239@docomo.ne.jp>
2025-09-07 18:39:10 +0900

    start

--- code_1.py
+++ code_1.py
@@ -1,2 +1,23 @@
 x = ['a','b','c','d','e']
 print(x[1:4])
+
+y = 9
+if y >= 10:
+    print('10以上')
+elif y >= 0:
+    print('0以上10未満')
+else:
+    print('0未満')
+
+class BodyCondition:
+    def bmi_calc(self):
+        print('あいうえお')
+
+bc = BodyCondition()
+bc.bmi_calc()
+
+x = '山田'
+y = '太郎'
+
+result = f'{x + y}さん'
+print(result)
